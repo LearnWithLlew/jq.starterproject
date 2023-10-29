ccf55cb2bbfc50ab68d41d08eb228b036810f082
Llewellyn Falco <llewellyn.falco@gmail.com>
2023-10-29 18:15:42 +0000

    . e created an install
    Co-Authored-By: blade290 <43077216+blade290@users.noreply.github.com>
    Co-Authored-By: Nitsan Avni <nitsanav@gmail.com>
    Co-Authored-By: Samuel Taggart <Samuel.Taggart@gmail.com>
    Co-Authored-By: Nazee Hajebi <2491283+nazeehajebi@users.noreply.github.com>
    Co-Authored-By: tegzy <52664058+tegzy@users.noreply.github.com>

--- install.windows.ps1
+++ install.windows.ps1
@@ -1,18 +1,18 @@
 # To run this script directly, run this in an elevated admin PowerShell prompt:
-#     Invoke-WebRequest -UseBasicParsing https://raw.githubusercontent.com/LearnWithLlew/yourlanguage.starterproject/main/install.windows.ps1 | Invoke-Expression
+#     Invoke-WebRequest -UseBasicParsing https://raw.githubusercontent.com/LearnWithLlew/jq.starterproject/main/install.windows.ps1 | Invoke-Expression
 
 # This script is intended to setup a dev machine from scratch. Very useful for setting up a EC2 instance for mobbing.
 #
 
 
-iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/windows.ps1 | iex
+#iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/windows.ps1 | iex
 
-choco install nodejs
+choco install jq
 refreshenv
 
 # Clone repo
-& "C:\Program Files\Git\cmd\git.exe" clone https://github.com/LearnWithLlew/yourlanguage.starterproject.git C:\Code\yourlanguage.starterproject
-cd C:\Code\yourlanguage.starterproject
+& "C:\Program Files\Git\cmd\git.exe" clone https://github.com/LearnWithLlew/jq.starterproject.git C:\Code\jq.starterproject
+cd C:\Code\jq.starterproject
 
 # initialize and run tests
 run_tests.bat
