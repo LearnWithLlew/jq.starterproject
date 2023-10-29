609a6de9e483e136ca89866c2e5f016af3ed1974
Llewellyn Falco <llewellyn.falco@gmail.com>
2023-10-29 19:27:05 +0000

    . e test runner fails / passes with message
    Co-Authored-By: blade290 <43077216+blade290@users.noreply.github.com>
    Co-Authored-By: Nitsan Avni <nitsanav@gmail.com>
    Co-Authored-By: Samuel Taggart <Samuel.Taggart@gmail.com>
    Co-Authored-By: Nazee Hajebi <2491283+nazeehajebi@users.noreply.github.com>
    Co-Authored-By: T. E. Green <78671457+tegsy@users.noreply.github.com>

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 jq -L . -nrf hello-world-test.jq > received.txt
-diff received.txt approved.txt
+diff received.txt approved.txt && echo "Tests Pass"  || ( echo "Tests Failed" && false )
